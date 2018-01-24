3094ea54b48d12b33f3489cae823cf34793cee38
ahmet faruk aygün <faygun89@gmail.com>
2018-01-24 11:36:38 +0300

    add swagger ui

--- TeaCounter.Service/Controllers/ProductController.cs
+++ TeaCounter.Service/Controllers/ProductController.cs
@@ -8,6 +8,7 @@ using System.Web.Http;
 using Newtonsoft.Json;
 using TeaCounter.Business.Interfaces;
 using TeaCounter.Global.Models.ServiceModels;
+using Swagger.Net.Annotations;
 
 namespace TeaCounter.Service.Controllers
 {
@@ -20,11 +21,42 @@ namespace TeaCounter.Service.Controllers
         {
             _productService = productService;
         }
-
-        public IHttpActionResult GetProduct()
+        /// <summary>
+        /// GetProduct method id is productId
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        /// <response code="200"></response>
+        [HttpGet]
+        [SwaggerOperation("GetProduct")]
+        public IHttpActionResult GetProduct(int id)
         {
             List<ProductResponseModel> products = _productService.GetProduct(1);
             return Ok(products);
+        }
+        /// <summary>
+        /// AllProduct method
+        /// </summary>
+        /// <returns></returns>
+        /// <response code="200"></response>
+        [HttpGet]
+        public IHttpActionResult AllProduct()
+        {
+            List<ProductResponseModel> products = _productService.GetProduct(1);
+            return Ok(products);
+        }
+
+        /// <summary>
+        /// AddProduct method name is product name and price is product price
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="price"></param>
+        /// <returns></returns>
+        /// <response code="200"></response>
+        [HttpPost]
+        public IHttpActionResult AddProduct (string name, double price)
+        {
+            return Ok(true);
         }
     }
 }
